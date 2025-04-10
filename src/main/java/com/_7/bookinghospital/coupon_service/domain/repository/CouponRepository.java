cae75c7b6bb1440ecf1f46acd7fb50ef58a9cef0
Jjini <sojinnuna@gmail.com>
2025-04-10 15:38:54 +0900

    feat: repository, mapper 생성 #3

--- src/main/java/com/_7/bookinghospital/coupon_service/domain/repository/CouponRepository.java
+++ src/main/java/com/_7/bookinghospital/coupon_service/domain/repository/CouponRepository.java
@@ -1,4 +1,13 @@
 package com._7.bookinghospital.coupon_service.domain.repository;
 
-public class CouponRepository {
+import com._7.bookinghospital.coupon_service.domain.model.Coupon;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CouponRepository {
+    List<Coupon> findAllAvailable();
+    Optional<Coupon> findById(UUID id);
+    void deleteById(UUID id);
+    Coupon save(Coupon coupon);
 }
--- src/main/java/com/_7/bookinghospital/coupon_service/infrastructure/repository/jpa/CouponJpaRepository.java
+++ src/main/java/com/_7/bookinghospital/coupon_service/infrastructure/repository/jpa/CouponJpaRepository.java
@@ -3,7 +3,9 @@ package com._7.bookinghospital.coupon_service.infrastructure.repository.jpa;
 import com._7.bookinghospital.coupon_service.domain.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, UUID> {
+    List<Coupon> findByIsDeletedFalse();
 }
